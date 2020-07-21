8d61705edcc5d165a9a8e4c6f949a54034e97587
lei <su321752a@gmail.com>
2020-07-21 17:21:55 +0800

    create

---
+++ k8s-lab-code/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace k8s_lab_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer t = new Timer(TimerCallback, null, 0, 2000);
+            Console.ReadKey();
+        }
+
+        public static void TimerCallback(object obj)
+        {
+            Console.WriteLine("show mes: " + DateTime.Now);
+            GC.Collect();
+        }
+    }
+}
